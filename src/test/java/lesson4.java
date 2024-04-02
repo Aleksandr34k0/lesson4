e6e402012de655740378774f913a0ad1582681f8
melnikov.aleksandr.a <z-me-y@mail.ru>
2024-04-02 15:43:25 +0300

    done test

--- src/test/java/lesson4.java
+++ src/test/java/lesson4.java
@@ -27,20 +27,36 @@ public class lesson4 {
 
     $("#query-builder-test").setValue("Selenide").pressEnter();
 
-    sleep(7000);
+    //sleep(7000);
 
 
         $(".Box-sc-g0xbh4-0.bBwPjs").click();
 
-        sleep(7000);
+    //    sleep(7000);
         $("#wiki-tab").click();
-        sleep(7000);
+    //    sleep(7000);
 
-        $("div.wiki-rightbar").$(byText("SoftAssertions")).click();
+
+
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+
+        $(byText("SoftAssertions")).click();
+
 
         $("#wiki-body").shouldHave(text("JUnit5 extension"));
 
-       // $(".internal present").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("""
+               @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }"""));
 
 
 
